02e715ad208c0775145271b60b6a6930afc5b3b9
Syndee <Cynthiaineh02@gmail.com>
2020-05-12 07:59:17 +0100

    Updated Content

--- main.js
+++ main.js
@@ -9,4 +9,6 @@ if (atposition<1 || dotposition<atposition+2 || dotposition+2>=x.length){
   alert("Please enter a valid e-mail address \n atpostion:"+atposition+"\n dotposition:"+dotposition);  
   return false;  
   }  
+
+  
 }  
