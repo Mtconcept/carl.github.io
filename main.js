1fbd66ec5f64dd640c4e042a88c8695f6e434bd6
Mt Concept <tayoolakunle77@gmail.com>
2020-05-17 19:08:39 +0100

    info

--- main.js
+++ main.js
@@ -1,12 +1,7 @@
-updateUser = document.getElementById(updates).values();
 
-function validateemail()  
-{  
-var x=document.myform.email.value;  
-var atposition=x.indexOf("@");  
-var dotposition=x.lastIndexOf(".");  
-if (atposition<1 || dotposition<atposition+2 || dotposition+2>=x.length){  
-  alert("Please enter a valid e-mail address \n atpostion:"+atposition+"\n dotposition:"+dotposition);  
-  return false;  
-  }  
-}  
+function onClickMenu(){
+	document.getElementById("menu").classList.toggle("change");
+	document.getElementById("nav").classList.toggle("change");
+	
+	document.getElementById("menu-bg").classList.toggle("change-bg");
+}
